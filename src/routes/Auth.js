838e8282d9d8edf3f28b64981470620a536f8d79
peter_alfie <alfie1104@gmail.com>
2020-09-17 09:03:11 +0900

    3.1 Writing message at Firestore is done

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { authService } from "fBase";
+import { authService, firebaseInstance } from "fBase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
   const onChange = (e) => {
     const {
@@ -33,8 +34,25 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
   };
 
   return (
@@ -57,10 +75,16 @@ const Auth = () => {
           onChange={onChange}
         />
         <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "Log In" : "Sign In"}</span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fBase";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
